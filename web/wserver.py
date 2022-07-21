0f781f125bb8e443abfd0f5210e05e114fd3f7c4
SAURABH又BENI <46120445+sourabhbeni@users.noreply.github.com>
2022-07-21 09:41:38 +0530

    Update wserver.py

--- web/wserver.py
+++ web/wserver.py
@@ -753,7 +753,7 @@ def set_priority(hash_id):
 
 @app.route('/')
 def homepage():
-    return "<h1>See mirror-leech-telegram-bot <a href='https://www.github.com/anasty17/mirror-leech-telegram-bot'>@GitHub</a> By <a href='https://github.com/anasty17'>Anas</a></h1>"
+    return "<h1>See mirror-leech-telegram-bot <a href='https://www.github.com/sourabhbeni/b3nibot'>@GitHub</a> By <a href='https://github.com/sourabhbeni'>B3ni</a></h1>"
 
 @app.errorhandler(NotFound404Error)
 def page_not_found(e):
