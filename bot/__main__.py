1c16dd7aee6a900989b355eb9375ad41e41da09d
SAURABH又BENI <46120445+sourabhbeni@users.noreply.github.com>
2022-07-21 09:42:55 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -67,8 +67,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("Repo", "https://www.github.com/sourabhbeni/b3nibot")
+    buttons.buildbutton("Owner", "https://t.me/@gc_anon")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
